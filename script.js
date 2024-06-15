0b87e1ac868fbdb04c3ceec87d304b0bdb32d2d1
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-06-15 15:12:19 +0530

    portfolio restart 1.0.0

--- script.js
+++ script.js
@@ -1,122 +1,136 @@
-// var tl = gsap.timeline();
+// // var tl = gsap.timeline();
 
-// tl.to("#box", {
-//   opacity: 0.5,
-//   x: 500,
-//   rotation: 360,
+// // tl.to("#box", {
+// //   opacity: 0.5,
+// //   x: 500,
+// //   rotation: 360,
+// //   delay: 1,
+// //   duration: 1,
+// // //   repeat: -100,
+// // //   yoyo: true,
+// //   scale: 8,
+// //   borderRadius: "50%",
+// //   backgroundColor: "purple",
+// // });
+
+// // tl.from("h1", {
+// //   duration: 1,
+// //   opacity: 0,
+// //   y: 30,
+// //   color: "purple",
+// //   stagger: 0.3,
+// // });
+
+
+// //nav
+
+// var main = document.querySelector("#page1");
+// var cursor = document.querySelector("#cursor");
+
+// main.addEventListener("mousemove", function (dets) {
+//   gsap.to("#cursor", {
+//     y: dets.y,
+//     x: dets.x,
+  
+  
+//   });
+ 
+// })
+
+
+
+
+// // page1
+// gsap.to("#name2", {
+//   y: -1600,
 //   delay: 1,
-//   duration: 1,
-// //   repeat: -100,
-// //   yoyo: true,
-//   scale: 8,
-//   borderRadius: "50%",
-//   backgroundColor: "purple",
-// });
-
-// tl.from("h1", {
-//   duration: 1,
-//   opacity: 0,
-//   y: 30,
-//   color: "purple",
-//   stagger: 0.3,
-// });
+//   duration: 2,
 
 
-//nav
-
-
-
-// page1
-gsap.to("#name2", {
-  y: -1600,
-  delay: 1,
-  duration: 2,
-
-
-  scrollTrigger: {
-    trigger: "#name1",
-    scroller: "body",
-    start: "top 10%",
-    scrub: true,
-  },
-});
-
-gsap.to("#name1", {
-  y: 1000,
-  delay: 1,
-  duration: 2,
-
-  scrollTrigger: {
-    trigger: "#name1",
-    scroller: "body",
-    start: "top 10%",
-    scrub: true,
-  },
-});
-
-// gsap.to("#image", {
-//     scale: 5,
-//     x: 500,
-//     y:500,
-//      scrollTrigger: {
-//     trigger: "#page2",
+//   scrollTrigger: {
+//     trigger: "#name1",
 //     scroller: "body",
-//     start: "top 100%",
+//     start: "top 10%",
 //     scrub: true,
 //   },
 // });
 
-gsap.to("#job", {
-  opacity: 0,
-  x: 350,
-  color: "white",
-  scrollTrigger: {
-    trigger: "#job",
-    scroller: "body",
-    start: "top 10%",
-    scrub: true,
-  },
-});
+// gsap.to("#name1", {
+//   y: 1000,
+//   delay: 1,
+//   duration: 2,
 
-gsap.to("#job1", {
-  opacity: 0,
-  x: 350,
-    scale: 3,
-  color:"white",
-  scrollTrigger: {
-    trigger: "#job1",
-    scroller: "body",
-    start: "top 10%",
-    scrub: true,
-  },
-});
+//   scrollTrigger: {
+//     trigger: "#name1",
+//     scroller: "body",
+//     start: "top 10%",
+//     scrub: true,
+//   },
+// });
 
-gsap.to("nav ul", {
-  opacity: 0,
-  y:30,
+// // gsap.to("#image", {
+// //     scale: 5,
+// //     x: 500,
+// //     y:500,
+// //      scrollTrigger: {
+// //     trigger: "#page2",
+// //     scroller: "body",
+// //     start: "top 100%",
+// //     scrub: true,
+// //   },
+// // });
 
-  color: "white",
-  scrollTrigger: {
-    trigger: "#job1",
-    scroller: "body",
-    start: "top 10%",
-    scrub: true,
-  },
-});
+// gsap.to("#job", {
+//   opacity: 0,
+//   x: 350,
+//   color: "white",
+//   scrollTrigger: {
+//     trigger: "#job",
+//     scroller: "body",
+//     start: "top 10%",
+//     scrub: true,
+//   },
+// });
 
-gsap.from("#navBar", {
+// gsap.to("#job1", {
+//   opacity: 0,
+//   x: 350,
+//     scale: 3,
+//   color:"white",
+//   scrollTrigger: {
+//     trigger: "#job1",
+//     scroller: "body",
+//     start: "top 10%",
+//     scrub: true,
+//   },
+// });
 
-    y: -650,
-    x: 650,
-    opacity:0,
+// gsap.to("nav ul", {
+//   opacity: 0,
+//   y:30,
+
+//   color: "white",
+//   scrollTrigger: {
+//     trigger: "#job1",
+//     scroller: "body",
+//     start: "top 10%",
+//     scrub: true,
+//   },
+// });
+
+// gsap.from("#navBar", {
+
+//     y: -650,
+//     x: 650,
+//     opacity:0,
  
   
 
-  color: "white",
-  scrollTrigger: {
-    trigger: "#job1",
-    scroller: "body",
-    start: "top 10%",
-    scrub: true,
-  },
-});
+//   color: "white",
+//   scrollTrigger: {
+//     trigger: "#job1",
+//     scroller: "body",
+//     start: "top 10%",
+//     scrub: true,
+//   },
+// });
